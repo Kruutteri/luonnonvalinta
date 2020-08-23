5af1d323ee08df9fef9e2aee8cc1a0ad98bb313a
Kruutteri <46073141+Kruutteri@users.noreply.github.com>
2020-08-23 21:03:56 +0300

    Add files via upload
    Mutaatiot toimivat

--- luonnonvalinta_simulaattori.py
+++ luonnonvalinta_simulaattori.py
@@ -1,19 +1,16 @@
 import random
 import matplotlib.pyplot as plt
 
-#attack,defence,koko,liha/kasvisyöjä,uros/naaras
-
-def eläin():
+def eliö():
     #jos speed on iso, ravinnontarve on suurempi (kuluttaa enemmän)
-    speed = random.randint(1,9)
+    #speed = random.randint(1,9)
+    speed = 5
     #mitä pienempi energia, sitä nopeammin eläimeltä loppuu energia ja se kuolee
-    energia = 30-speed*2
-    #jos näköaisti on iso, näkee ravinnon ja muut eläimet kauempaa
-    #näköaisti = 10-speed
+    energia = energia_alotusarvo - speed*energia_kerroin
     
-    eläin = [speed,energia]
+    eliö = [speed,energia]
 
-    return eläin
+    return eliö
     
 
 
@@ -28,12 +25,12 @@ def setup(koko):
                 
     animals = {}
     for i in range(alkupopulaatio):
-        animals["eläin"+str(i)] = eläin()
+        animals["eliö"+str(i)] = eliö()
 
 
     for i in range(int(koko*0.8)):
         for j in range(int(koko*0.8)):
-            if random.randint(1,10) < 3:
+            if random.randint(1,10) < ruuan_määrä:
                 grid[i][j] = 1
 
     return animals,grid
@@ -42,10 +39,12 @@ def setup(koko):
 koko = 100
 
 ##################################
-päivä = 300
-monta_päivää = 50
+päivä = 300 #päivän pituus
+monta_päivää = 150 #monta päivää simuloidaan maksimissaan
 alkupopulaatio = 100
-kuinka_usein_printtaa = 100
+energia_alotusarvo = 30
+energia_kerroin = 2 #mitä isompi luku, sitä isompi vaikutus nopeudella on energiaan
+ruuan_määrä = 3 #1-10, 1=10%, 10=100% kentästä on ruokaa
 ##################################
 
 animals,grid = setup(koko)
@@ -81,11 +80,8 @@ for xx in range(monta_päivää):
             
         #yksi episode (päivä)
         for t in range(päivä):
-            #print(animals[animal])
-
-            #animals[animal][0] = nopeus
             for i in range(int(animals[animal][0])+int(animals[animal][1])):
-                
+                #arvotaan mihin suuntaan eliö kulkee (eliöiden liike satunnaista)
                 suunta = random.randint(1,4)
                 
                 #vasen
@@ -171,7 +167,9 @@ for xx in range(monta_päivää):
                                     animals[animal][-1] -= 1
                                     animals[animal].remove(animals[animal][-2])
 
-    
+
+
+    #katsotaan mitkä eliöt ansaitsevat kuolla ja mitkä lisääntyä
     poistolista = []
     lisääntymislista = []
     for animal in animals:
@@ -188,15 +186,36 @@ for xx in range(monta_päivää):
         else:
             poistolista.append(animal)
     
-    #tapetaan eläimet
+    #tapetaan huonot eliöt
     for animal in poistolista:
         if animal in animals:
             animals.pop(animal,None)
 
-    #lisäännytetään valioyksilöt
+    #lisäännytetään valioyksilöt ja pieni todennäköisyys mutaatiolle (10%)
     for animal in lisääntymislista:
         if animal in animals:
             animals[animal+"v"+str(xx)] = animals[animal]
+            arpa = random.randint(1,20)
+            if arpa < 18:
+                animals[animal+"v"+str(xx)] = animals[animal]
+
+            elif arpa == 19:
+                if animals[animal][0] == 9:
+                    animals[animal+"v"+str(xx)] = animals[animal]
+                else:
+                    animals[animal+"v"+str(xx)] = animals[animal]
+                    animals[animal+"v"+str(xx)][0] += 1
+                    animals[animal+"v"+str(xx)][1] = energia_alotusarvo - animals[animal+"v"+str(xx)][0]*energia_kerroin
+                    
+            elif arpa == 20:
+                if animals[animal][0] == 1:
+                    animals[animal+"v"+str(xx)] = animals[animal]
+                else:
+                    animals[animal+"v"+str(xx)] = animals[animal]
+                    animals[animal+"v"+str(xx)][0] -= 1
+                    animals[animal+"v"+str(xx)][1] = energia_alotusarvo - animals[animal+"v"+str(xx)][0]*energia_kerroin
+                    
+
 
     #arvotaan uudet ruuat
     grid = []
@@ -209,37 +228,37 @@ for xx in range(monta_päivää):
                 
     for i in range(int(koko*0.8)):
         for j in range(int(koko*0.8)):
-            if random.randint(1,10) < 3:
+            if random.randint(1,10) < ruuan_määrä:
                 grid[i][j] = 1
                 
     
-    #tyhjennetään eläinten vatsat
+    #tyhjennetään eliöiden vatsat
     for animal in animals:
         animals[animal] = animals[animal][:2]
 
-##    if xx%kuinka_usein_printtaa == 0:
-##        print(len(animals))
 
+    #tehdään lista eliöiden speed-statseista    
     listax = []
     for animal in animals:
         listax.append(animals[animal][0])
 
-    plt.xlim([1,9])
-    plt.hist(listax,5)
+    plt.xlim([1,9]) #x-akselin arvot (plot x-akseli ei nyt muutu itsestään)
+    plt.hist(listax,10)
     plt.title("Day: "+str(xx))
     plt.xlabel("Speed")
     plt.ylabel("Population size")
-    plt.pause(0.05)
-    plt.clf()
-    plt.draw()
+    plt.pause(0.05) #pakollinen jos haluaa käyttää plt.draw()
+    plt.clf() #clearataan plot
+    plt.draw() #plt.draw() piirtää vanhan plotin päälle
 
 
+#tämä vielä kerran, jotta saadaan kaavio pysymään main loopin loputtua
 listax = []
 for animal in animals:
     listax.append(animals[animal][0])
 
 plt.xlim([1,9])
-plt.hist(listax,5)
+plt.hist(listax,10)
 plt.title("Day: "+str(xx))
 plt.xlabel("Speed")
 plt.ylabel("Population size")
